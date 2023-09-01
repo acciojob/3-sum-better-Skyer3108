86fd957cc20b56399b7cb5905c646c502ff89069
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2023-09-01 13:27:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum=0;
+  for(let i=0; i<arr.length; i++){
+              sum+=arr[i];
+	
+  }
+	if(sum==target){
+		console.log(sum)
+	}
 }
 
 module.exports = threeSum;
